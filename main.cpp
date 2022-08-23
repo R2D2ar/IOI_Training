e74c06c688f1c6ca62c49b9ca38ba95e39053b94
Artur <62879488+R2D2ar@users.noreply.github.com>
2022-08-23 07:19:45 +0200

    Improved

--- main.cpp
+++ main.cpp
@@ -1,31 +1,5 @@
 #include <bits/stdc++.h>
 
-bool existing(const std::vector<std::pair<int, int>>& input, int number){
-    if(!input.empty()){
-        for (auto item : input) {
-            if(item.first == number) return true;
-        }
-    }
-    return false;
-}
-
-int find_first(const std::vector<std::pair<int, int>>& input, int number){
-    int index = 0;
-    for (auto item : input) {
-        if(item.first == number) return index;
-        index++;
-    }
-    return -1;
-}
-
-int smallest_number_friends(const std::vector<std::pair<int, int>>& input){
-    int smallest = 1000000;
-    for (auto item : input) {
-        if (item.second < smallest) smallest = item.second;
-    }
-    return smallest;
-}
-
 int main() {
     int amount_users;
     std::cin >> amount_users;
@@ -34,23 +8,34 @@ int main() {
     std::cin >> amount_friends;
 
     int current_input = 0;
-    std::vector<std::pair<int, int>> output;
+    int input[amount_friends*2];
     for (int i = 0; i < amount_friends * 2; ++i) {
         std::cin >> current_input;
-        if(!existing(output, current_input)) {
-            output.emplace_back(current_input, 1);
-        }
-        else{
-            int index = find_first(output, current_input);
-            output[index].second += 1;
+        input[i] = current_input;
+    }
+
+    std::sort(input, input + sizeof input / sizeof input[0]);
+
+    int position = 0;
+    int counter = 0;
+    int output[amount_users];
+    for(int i = 0; i < amount_friends * 2; ++i){
+        counter++;
+        if ((i+1 < amount_friends * 2 && input[i] != input[i+1]) || i == amount_friends * 2 - 1){
+            output[position] = counter;
+            counter = 0;
+            position++;
         }
     }
 
-    std::sort(output.begin(), output.end());
-    int smallest = smallest_number_friends(output);
-    for (auto item : output) {
-        if(item.second == smallest){
-            std::cout << item.first << " ";
+    int smallest = 10000;
+    for (int i = 0; i < amount_users; ++i) {
+        if(output[i] < smallest) smallest = output[i];
+    }
+
+    for (int i = 0; i < amount_users; ++i) {
+        if (output[i] == smallest){
+            std::cout << i << " ";
         }
     }
 
